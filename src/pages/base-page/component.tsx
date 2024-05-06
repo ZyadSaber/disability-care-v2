2b01fd302c90f21e6fcb1c7c8a87864c199b4b4c
Zyad Saber <zyad.2002.best@gmail.com>
2024-05-07 02:27:10 +0300

    update on the nav

--- src/pages/base-page/component.tsx
+++ src/pages/base-page/component.tsx
@@ -1,12 +1,40 @@
-import { memo } from "react";
+import { memo, useState, useEffect } from "react";
 import { Outlet } from "react-router-dom"
+import Header from "./partials/Header"
 
 const BasePage = memo(() => {
+    const [lastScroll, setLastScroll] = useState(0);
+    const [scrollClass, setScrollClass] = useState<string>();
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScroll = window.pageYOffset;
+
+            if (currentScroll <= 0) {
+                setScrollClass("");
+                return;
+            }
+
+            if (currentScroll > lastScroll && scrollClass !== "scroll-down") {
+                setScrollClass("scroll-down");
+            } else if (currentScroll < lastScroll && scrollClass === "scroll-down") {
+                setScrollClass("scroll-up");
+            }
+
+            setLastScroll(currentScroll);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [lastScroll, scrollClass]);
     return (
-        <>
-            div
-            <Outlet />
-        </>
+        <div className={`${scrollClass}`}>
+            <Header />
+            <div className="mt-16">
+                <Outlet />
+            </div>
+        </div>
     )
 })
 
---
+++ src/pages/base-page/partials/Header.tsx
@@ -0,0 +1,26 @@
+import { memo } from "react";
+import { Link } from "react-router-dom";
+
+const Header = memo(() => {
+
+    return (
+        <header className="fixed top-0 left-0 w-full py-5 text-center transition-all h-16 z-40 flex justify-around items-center bg-blue-800">
+            <Link to="/">
+                <h2 className="text-white font-medium text-2xl">Max <span className="text-yellow-400">Care</span></h2>
+            </Link>
+            <div className="gap-5 text-white hidden md:flex">
+                <Link to="#" className="hover:text-red-500">Home</Link>
+                <Link to="#" className="hover:text-red-500">Book</Link>
+                <Link to="#" className="hover:text-red-500">Discharges</Link>
+                <Link to="#" className="hover:text-red-500">About Us</Link>
+            </div>
+            <div className="w-8 flex-wrap justify-end flex md:hidden">
+                <span className="bg-white mb-1.5 h-0.5 w-full"></span>
+                <span className="bg-white mb-1.5 h-0.5 w-full"></span>
+                <span className="bg-white mb-1.5 h-0.5 w-full"></span>
+            </div>
+        </header>
+    )
+});
+
+export default Header;
