ea2969c04d272a0182b20c8b2bbfa75597423d96
Zyad Saber <zyad.2002.best@gmail.com>
2024-06-21 02:44:37 +0300

    coming soon page

--- src/pages/home-page/component.tsx
+++ src/pages/home-page/component.tsx
@@ -1,6 +1,7 @@
 import { memo, useState, useLayoutEffect, useRef, useCallback } from "react";
 import { Link } from "react-router-dom"
-import { slider1, slider2, slider3, slider4, van, wheelChair, arrows, services, p1, p2, p3, step1, step2 } from "../../images";
+import { slider1, slider2, slider3, slider4, p2 } from "../../images";
+import { servicesItems } from "./constants"
 import {
     SliderContainer,
     SliderWrapper,
@@ -64,7 +65,10 @@ const HomePage = memo(() => {
 
     return (
         <>
-            <SliderContainer>
+            <div className="h-full">
+                d
+            </div>
+            {/* <SliderContainer>
                 <SliderWrapper className="flex" ref={SliderContainerRef} translateX={currentTransition}>
                     {images.map((image, index) => (
                         <img key={index} src={image} className="min-w-full" />
@@ -80,69 +84,35 @@ const HomePage = memo(() => {
                 </ButtonsWrapper>
             </SliderContainer>
 
-            <img src={services} alt="" className="bg-blue-800" />
-
             <h4 className="w-full text-center pt-5 px-5 md:px-32 text-5xl font-normal">Our Services</h4>
 
             <div className=" flex gap-2 py-5 px-5 md:px-32 flex-col md:flex-row">
-                <div className=" w-full md:w-4/12 flex flex-col justify-center items-center p-5 gap-3">
-                    <img src={van} alt="" className="max-h-52" />
-                    <h4 className="text-2xl font-semibold">Wheelchair Transport</h4>
-                    <p className="font-light">With our ambulatory transportation, you can confidently move toward your desired destinations. Our reliable and courteous drivers offer assistance and support, ensuring a seamless journey for those who can walk independently but require transportation to medical facilities, events, or any essential appointments. </p>
-                    <Link to="" className="px-6 py-2 bg-blue-700 hover:bg-blue-400 transition-all border rounded-md text-1xl font-medium text-white">Read More</Link>
-                </div>
-                <div className=" w-full md:w-4/12 flex flex-col justify-center items-center p-5 gap-3">
-                    <img src={wheelChair} alt="" className="max-h-52" />
-                    <h4 className="text-2xl font-semibold">Stretcher Transport</h4>
-                    <p className="font-light">With our ambulatory transportation, you can confidently move toward your desired destinations. Our reliable and courteous drivers offer assistance and support, ensuring a seamless journey for those who can walk independently but require transportation to medical facilities, events, or any essential appointments. </p>
-                    <Link to="" className="px-6 py-2 bg-blue-700 hover:bg-blue-400 transition-all border rounded-md text-1xl font-medium text-white">Read More</Link>
-                </div>
-                <div className=" w-full md:w-4/12 flex flex-col justify-center items-center p-5 gap-3">
-                    <img src={arrows} alt="" className="max-h-52" />
-                    <h4 className="text-2xl font-semibold">Ambulatory Transportation</h4>
-                    <p className="font-light">With our ambulatory transportation, you can confidently move toward your desired destinations. Our reliable and courteous drivers offer assistance and support, ensuring a seamless journey for those who can walk independently but require transportation to medical facilities, events, or any essential appointments. </p>
-                    <Link to="" className="px-6 py-2 bg-blue-700 hover:bg-blue-400 transition-all border rounded-md text-1xl font-medium text-white">Read More</Link>
-                </div>
+                {servicesItems.map(({ imageSrc, title, body, link }) => (
+                    <div className=" w-full md:w-4/12 flex flex-col justify-start items-center p-5 gap-3">
+                        <img src={imageSrc} alt="" className="max-h-52" />
+                        <h4 className="text-2xl font-semibold">{title}</h4>
+                        <p className="font-light">{body}</p>
+                        <Link to={link} className="px-6 py-2 mt-5 bg-blue-700 hover:bg-blue-400 transition-all border rounded-md text-1xl font-medium text-white">Read More</Link>
+                    </div>
+                ))}
             </div>
 
-            <h4 className="w-full text-center pt-5 px-5 md:px-32 text-5xl font-normal">WHO DO WE SERVE</h4>
-
-            <div className="py-5 px-5 md:px-32">
-                <div className="flex my-10 flex-col md:flex-row">
-                    <img src={p1} alt="" className="w-full md:w-6/12" />
-                    <div className="w-full md:w-6/12 flex flex-col items-center justify-center gap-3 p-10">
-                        <h4 className="text-2xl font-normal text-center">THE COMMUNITY OF JACKSONVILLE, FLORIDA</h4>
-                        <p className="text-center">At TruCare N.E.M.T. of Jax, our mission is to provide safe and reliable transportation to those who need it most. We cover =Duval, St. Johns, and clay counties in northeast Florida.</p>
-                        <p className="text-center">We are committed to being a caring and dedicated partner for our community, always on schedule, and focused on delivering exceptional services to empower lives. </p>
-                    </div>
-                </div>
-                <div className="flex my-10 flex-col md:flex-row-reverse">
-                    <img src={p2} alt="" className="w-full md:w-6/12" />
-                    <div className="w-full md:w-6/12 flex flex-col items-center justify-center gap-3 p-10">
-                        <h4 className="text-2xl font-normal text-center">AMBULATORY AND WHEELCHAIR CUSTOMERS</h4>
-                        <p className="text-center"> At TruCare N.E.M.T. of Jax we take pride in being the trusted partner for both ambulatory and wheelchair customers, empowering them to lead fulfilling lives and access the opportunities and services they need.</p>
-                        <p className="text-center">We strive to make transportation a worry-free and enjoyable experience for everyone we serve.</p>
-                    </div>
-                </div>
-                <div className="flex my-10 flex-col md:flex-row">
-                    <img src={p3} alt="" className="w-full md:w-6/12" />
-                    <div className="w-full md:w-6/12 flex flex-col items-center justify-center gap-3 p-10">
-                        <h4 className="text-2xl font-normal text-center">HEALTH CARE FACILITIES</h4>
-                        <p className="text-center"> Our specialized services cater to a wide range of health care facilities, including nursing homes, assisted living centers, rehabilitation centers, hospices, and medical clinics.</p>
-                        <p className="text-center">Whether it's transporting elderly residents, patients with medical conditions, or individuals with mobility challenges, our team of skilled drivers is trained to handle diverse situations with the utmost care and professionalism. </p>
+            <div className="flex flex-col pt-5 px-5 md:px-32 justify-center items-center">
+                <h4 className="w-full text-center pt-5 py-5 md:px-32 text-5xl font-normal">Our patient transport services include trips to/from</h4>
+                <div className="flex justify-center items-center gap-10">
+                    <div className="shadow-xl">
+                        dd
                     </div>
                 </div>
             </div>
 
-            <div className="flex flex-col bg-blue-500 text-white justify-center items-center py-5 px-5 md:px-32">
-                <h4 className="w-full text-center pt-5 px-5 md:px-32 text-5xl font-semibold text-white">Let’s get started!</h4>
-                <p className="text-slate-200 m-5 text-xl">Simply click the Trip Charge Estimator and put in the address – go anywhere. If you agree to the price, simply Request A Trip.</p>
-                <div className="flex justify-evenly items-center w-full flex-col md:flex-row gap-2">
-                    <img src={step1} alt="" />
-                    <img src={step2} alt="" />
+            <div className="flex flex-col pt-5 px-5 md:px-32 justify-center items-center">
+                <h4 className="w-full text-center pt-5 py-5 md:px-32 text-5xl font-normal">WHO DO WE SERVE</h4>
+                <div className="flex justify-center items-center gap-10">
+                    <img src={p2} alt="" className="w-1/3 rounded shadow-md" />
+                    <p className="w-2/3 text-lg"> Max Care Transportation provides non-emergency medical transportation for patients, family members, and caregivers throughout Northeast Florida. Our patient transportation answering service is available 24/7 to cater to your needs.</p>
                 </div>
-            </div>
-
+            </div> */}
         </>
     );
 });
---
+++ src/pages/home-page/constants.ts
@@ -0,0 +1,22 @@
+import { van, wheelChair, Stretcher } from "../../images";
+
+export const servicesItems = [
+  {
+    imageSrc: van,
+    title: "Ambulatory",
+    body: "Ambulatory transportation is the most common form of non-emergency medical transportation. It is for patients who do not need equipment to travel and who are able to walk, or only need basic assistance.",
+    link: "",
+  },
+  {
+    imageSrc: wheelChair,
+    title: "Wheelchair",
+    body: "Wheelchair transport services offer help to those who are confined to a wheelchair or who cannot walk on their own. CPR-trained drivers operate the vans and wheelchair lifts. Wheelchairs are available for rent and special consideration is given to patients with disabilities. Again, the use of this service implies that the patient does not need immediate emergency care at the time of service.",
+    link: "",
+  },
+  {
+    imageSrc: Stretcher,
+    title: "Stretcher",
+    body: "stretcher medical transport is available, specifically for patients who suffer from back conditions or who cannot sit upright. Scheduling this service implies that emergency assistance is not required in transit or at time of arrival.",
+    link: "",
+  },
+];
